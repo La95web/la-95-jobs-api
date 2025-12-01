1dc3b1281df2564fb414105c15cdeb8da1767356
louis17782 <luissanteliz22@gmail.com>
2025-12-01 11:10:09 -0400

    changes port

--- src/index.ts
+++ src/index.ts
@@ -150,8 +150,8 @@ app.post('/comments', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log('Server is running on http://localhost:3001');
 });
